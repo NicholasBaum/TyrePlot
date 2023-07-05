a63fc7d0c473e1cb28312b36fc0a4e049f2322aa
NB <n.baumgaertner@hotmail.com>
2023-07-05 19:03:36 +0200

    ups

--- plotter.js
+++ plotter.js
@@ -1,22 +1,45 @@
 class LineData {
-    constructor(name, yData) {
+    constructor(id, name, yData) {
+        this.id = id;
         this.name = name;
         this.yData = yData;
+        this.visible = true;
+        this.color = null;
     }
 }
 
 class Plotter {
     constructor() {
         this.lines = [];
+        this.onChangedHandlers = [];
+    }
+
+    addOnChanged(callback) {
+        this.onChangedHandlers.push(callback);
+    }
+
+    removeOnChanged(callback) {
+        const index = this.onChangedHandlers.indexOf(callback);
+        if (index !== -1) {
+            this.onChangedHandlers.splice(index, 1);
+        }
+    }
+
+    raiseOnChanged() {
+        for (const c of this.onChangedHandlers) {
+            c();
+        }
     }
 
     addLine(lineData) {
         this.lines.push(lineData);
+        this.raiseOnChanged();
     }
 
     drawLine(lineData) {
         this.lines.push(lineData);
         this.render();
+        this.raiseOnChanged();
     }
 
     render() {
@@ -25,7 +48,7 @@ class Plotter {
         const w = 0.9 * el.offsetWidth;
         const h = 0.8 * el.offsetHeight;
         Plotly.newPlot("plotElement", /* JSON object */ {
-            "data": this.lines.map(x => ({ "y": x.yData })),
+            "data": this.lines.filter(x => x.visible).map(x => ({ "y": x.yData })),
             "layout": { "width": w, "height": h }
         })
     }
