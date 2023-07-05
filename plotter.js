764de49483ea65f5f20c6bb7666d65fd2fc83669
NB <n.baumgaertner@hotmail.com>
2023-07-05 19:55:27 +0200

    ups

--- plotter.js
+++ plotter.js
@@ -47,9 +47,37 @@ class Plotter {
         const el = document.getElementById('plotContainer');
         const w = 0.9 * el.offsetWidth;
         const h = 0.8 * el.offsetHeight;
-        Plotly.newPlot("plotElement", /* JSON object */ {
-            "data": this.lines.filter(x => x.visible).map(x => ({ "y": x.yData })),
-            "layout": { "width": w, "height": h }
-        })
+
+
+        const data = this.lines
+            .filter(x => x.visible)
+            .map(x => {
+                if (x.color === null)
+                    x.color = this.getNextColor();
+                return {
+                    // type: 'scatter',
+                    // x: [1, 2, 3, 4],
+                    y: x.yData,
+                    // mode: 'lines',
+                    name: x.name,
+                    line: {
+                        color: x.color,
+                        // width: 3
+                    }
+                };
+            });
+
+        const layout = {
+            "width": w,
+            "height": h
+        };
+
+        Plotly.newPlot("plotElement", { data, layout });
+    }
+
+    currentColorIndex = 0;
+    colorPalette = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"];
+    getNextColor() {
+        return this.colorPalette[this.currentColorIndex++ % this.colorPalette.length]
     }
 }
