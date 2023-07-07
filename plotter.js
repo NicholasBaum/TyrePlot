41465f08f758622408ff3fd276ce02512f960abf
NB <n.baumgaertner@hotmail.com>
2023-07-07 03:10:33 +0200

    ups

--- plotter.js
+++ plotter.js
@@ -64,24 +64,68 @@ class Plotter {
         this.render();
         this.raiseOnChanged();
     }
+    // render3() {
+    //     if (this.lines.length === 0) return;
+    //     const el = document.getElementById('plotContainer');
+    //     const w = 0.9 * el.offsetWidth;
+    //     const h = 0.8 * el.offsetHeight;
 
+
+    //     const data = this.lines
+    //         .filter(x => x.visible)
+    //         .map(x => {
+    //             if (x.color === null)
+    //                 x.color = this.getNextColor();
+    //             return {
+    //                 type: 'line',
+    //                 x: x.time,
+    //                 y: x.yData,
+    //                 //yaxis: `y${x.id + 1}`,
+    //                 // mode: 'lines',
+    //                 //name: `${x.channelName}<br>${x.info}`,
+    //                 name: x.channelName,
+    //                 line: {
+    //                     color: x.color,
+    //                     // width: 3
+    //                 }
+    //             };
+    //         });
+
+    //     const layout = {
+    //         "width": w,
+    //         "height": h,
+    //         xaxis: { title: 'time in s', showgrid: false, showline: true, zeroline: false },
+    //         yaxis: { title: 'y-axis 1', showline: false, zeroline: false, showgrid: false },
+    //         yaxis2: { title: 'y-axis 2', overlaying: 'y', side: 'right', showgrid: false }
+    //     };
+
+    //     Plotly.newPlot("plotElement", { data, layout });
+    // }
     render() {
         if (this.lines.length === 0) return;
         const el = document.getElementById('plotContainer');
         const w = 0.9 * el.offsetWidth;
         const h = 0.8 * el.offsetHeight;
 
+        const layout = {
+            "width": w,
+            "height": h,
+            xaxis: { title: 'seconds', showgrid: false, showline: true, zeroline: false },
+        };
 
         const data = this.lines
             .filter(x => x.visible)
             .map(x => {
+                let axis = this.getAxis(x.channelName)
+                layout[`yaxis${axis.number}`] = axis.config;
+                console.log(layout);
                 if (x.color === null)
                     x.color = this.getNextColor();
                 return {
                     type: 'line',
                     x: x.time,
                     y: x.yData,
-                    yaxis: `y${x.id + 1}`,
+                    yaxis: `y${axis.number}`, // strange logic, y2 corresponds to yaxis2 
                     // mode: 'lines',
                     //name: `${x.channelName}<br>${x.info}`,
                     name: x.channelName,
@@ -92,15 +136,24 @@ class Plotter {
                 };
             });
 
-        const layout = {
-            "width": w,
-            "height": h,
-            xaxis: { title: 'time in s', showgrid: false, showline: true, zeroline: false },
-            yaxis: { title: 'y-axis 1', showline: false, zeroline: false, showgrid: false },
-            yaxis2: { title: 'y-axis 2', overlaying: 'y', side: 'right', showgrid: false }
-        };
+
 
         Plotly.newPlot("plotElement", { data, layout });
+    }
+
+    axisInfos = {
+        default: { number: 3, config: { showline: false, zeroline: false, showgrid: false, overlaying: 'y', side: 'left' } },
+        Pa: { number: 2, config: { title: 'Pa', showline: false, zeroline: false, showgrid: false, overlaying: 'y', side: 'right' } },
+        C: { number: 1, config: { title: '°C', showline: false, zeroline: false, showgrid: false, side: 'left' } },
+    };
+    getAxis(channelName) {
+        if (channelName.startsWith('p')) {
+            return this.axisInfos['Pa'];
+        } else if (channelName.startsWith('TT')) {
+            return this.axisInfos['C'];
+        } else {
+            return this.axisInfos['default'];
+        }
     }
 }
 
